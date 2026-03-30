592635a99469e1387e90f9d5c3cc66e584270a36
amaanpathan2313 <amanpathan348@gmail.com>
2026-03-30 16:23:44 +0530

    Auth with JWT

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,50 +1,110 @@
-
 const AuthModel = require("../models/auth.model");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const express = require('express');
+ 
+const express = require("express");
 
 const authRoute = express.Router();
 
-authRoute.post('/sign-up', async(req, res) => {
+// sign-up
+authRoute.post("/sign-up", async (req, res) => {
+  const { name, phon, email, password } = req.body;
+  const { role } = req.body;
 
-    const {name, phon, email, password} = req.body;
-    const {role} = req.body;
+  console.log(password);
+  if (!name || !phon || !email || !password) {
+    res.status(406).json({ msg: "Fill All details correctly ! " });
+    return;
+  }
+  if (phon.length != 10) {
+    res.status(406).json({ msg: "Enter Correct Phon Number ! " });
+    return;
+  }
 
-    console.log(password)
-    if(!name || !phon || !email || !password){
-         res.status(406).json({msg : "Fill All details correctly ! "});
-         return;
+  try {
+    let existUser = await AuthModel.findOne({ email });
+
+    if (existUser) {
+      res
+        .status(406)
+        .json({ msg: `${email} is already register with system plz login` });
+      return;
     }
-    if(phon.length != 10){
-         res.status(406).json({msg : "Enter Correct Phon Number ! "});
-         return;
+
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+      // Store hash in your password DB.
+      if (err) {
+        res
+          .status(500)
+          .json({ msg: "Error occur while hashing the password", err });
+        return;
+      }
+      if (hash) {
+        await AuthModel.create({ name, phon, email, password: hash, role });
+
+        res
+          .status(201)
+          .json({ msg: `${email} you are successfully sign-up !` });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ msg: `Error occur while sign-up : ${err.message}` });
+  }
+});
+
+// login
+
+authRoute.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ msg: "All fields are required" });
+    return;
+  }
+
+  try {
+    let targetUser = await AuthModel.findOne({email});
+    console.log(targetUser._id)
+
+    if (!targetUser) {
+      res.status(404).json({ msg: `User not found with ${email} plz login !` });
+      return;
     }
 
-    try {
+    // Load hash from your password DB.
 
-        let existUser = await AuthModel.findOne({email});
+    let hash = targetUser.password;
+    bcrypt.compare(password, hash, function (err, result) {
+      // result == true
+      if(result){
 
-        if(existUser){
-            res.status(406).json({msg : `${email} is already register with system plz login`});
-            return;
+        var token = jwt.sign({ role : targetUser.role, id : targetUser._id, name : targetUser.name }, process.env.JWT_KEY);
+
+
+          res.status(200).json({msg : `${targetUser.name} successfully login !`, token });
+          
+        }else{
+          res.status(403).json({msg : "Wrong password plz try again ! "});
+      
         }
-
-        bcrypt.hash(password, saltRounds, function(err, hash) {
-            // Store hash in your password DB.
-           });
-
-         await AuthModel.create({name, phon, email, password, role});
-
-         res.status(201).json({msg : `${email} you are successfully sign-up !`})
-         
-        } catch (err) {
-
-        res.status(500).json({msg : `Error occur while sign-up : ${err.message}`});
         
-    }
-})
+        if(err){
+            res.status(500).json({msg : "Error occur while hashing the password !  ", err});
+       
+            
+        }
+        
+        
+    });
+} catch (err) {
+    
+    res.status(500).json({msg : "Error occur on sign up !  ", err});
+    
 
+  }
+});
 
 module.exports = authRoute;
